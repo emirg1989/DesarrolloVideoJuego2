5197cce45fba86676c904cc7e8c1e5f09b456b46
emirg1989 <eganimi1989@gmail.com>
2016-11-06 19:12:44 -0300

    tp3

--- UM_DVII_TP3/Assets/Scripts/models/IEnemyModel.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public interface IEnemyModel
-{
-    float speed { get; set; }
-}
--- UM_DVII_TP3/Assets/Scripts/models/IPlayerModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public interface IPlayerModel
-{
-
-    float speed { get; set; }
-}
